a714f09d0ae010578e6d4ef9f97515dc1564b737
adedamola26 <adesoye.a@northeastern.edu>
2025-03-30 15:06:22 -0400

    done with draft of first two sections

--- src/app/features/web/pages/home/home.component.ts
+++ src/app/features/web/pages/home/home.component.ts
@@ -1,17 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-home',
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+    selector: 'app-home',
+    imports: [CommonModule, RouterModule],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss',
 })
 export class HomeComponent implements OnInit {
-
     constructor() {}
 
     ngOnInit(): void {
         console.log('Home Component');
     }
-
 }
