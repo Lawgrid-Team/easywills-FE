462f1120e9669186562acae479a2168c17ab1f94
adedamola26 <adesoye.a@northeastern.edu>
2025-07-22 07:35:42 -0400

    drafted the subscription page.

--- src/app/features/dashboard/settings/subscription-management/subscription-management.component.ts
+++ src/app/features/dashboard/settings/subscription-management/subscription-management.component.ts
@@ -1,11 +1,49 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-subscription-management',
-  imports: [],
-  templateUrl: './subscription-management.component.html',
-  styleUrl: './subscription-management.component.scss'
+    selector: 'app-subscription-management',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './subscription-management.component.html',
+    styleUrl: './subscription-management.component.scss',
 })
 export class SubscriptionManagementComponent {
-
+    billingHistory = [
+        {
+            date: '27/07/2025',
+            status: 'Successful',
+            amount: '100,000',
+            plan: 'Legacy',
+            method: 'mastercard',
+        },
+        {
+            date: '20/06/2025',
+            status: 'Successful',
+            amount: '100,000',
+            plan: 'Legacy',
+            method: 'mastercard',
+        },
+        {
+            date: '16/05/2025',
+            status: 'Unsuccessful',
+            amount: '100,000',
+            plan: 'Legacy',
+            method: 'mastercard',
+        },
+        {
+            date: '10/03/2025',
+            status: 'Unsuccessful',
+            amount: '100,000',
+            plan: 'Legacy',
+            method: 'mastercard',
+        },
+        {
+            date: '6/02/2025',
+            status: 'Successful',
+            amount: '100,000',
+            plan: 'Legacy',
+            method: 'mastercard',
+        },
+    ];
 }
